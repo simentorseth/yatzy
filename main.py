b2a86beb0f93e9fc76d350c59423fcd03c077795
Tørseth <simen.torseth@sweco.no>
2026-09-10 08:55:56 +0200

    intital commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def main():
+    yatzy-loppen er som følger:
+
+    1. trill 6 terninger
+    2. spar på ønskede øyne
+    3. gjenta enten 2 ganger til og sett ønskede øyne på en post
+        eller sett ønskede øyne på en post før alle kast er tatt og motta ekstra kast
+
+
+if "__name__" == "__main__":
+    main()

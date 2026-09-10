eac1a127cb2485d69e0fc54168404cdbf4b04278
Tørseth <simen.torseth@sweco.no>
2026-09-10 10:12:44 +0200

    testing rolling and keeping dice

--- main.py
+++ main.py
@@ -1,11 +1,44 @@
+import numpy as np
+
+
 def main():
-    yatzy-loppen er som følger:
+    seed = 1
+    np.random.seed(seed)
 
-    1. trill 6 terninger
-    2. spar på ønskede øyne
-    3. gjenta enten 2 ganger til og sett ønskede øyne på en post
-        eller sett ønskede øyne på en post før alle kast er tatt og motta ekstra kast
+    dice = np.array([i for i in range(1, 7)])
 
+    # first roll
+    print("1. roll!")
+    roll = np.zeros(6, dtype=int)
+    for i in range(len(dice)):
+        die = np.random.choice(dice)
+        roll.put(i, die)
 
-if "__name__" == "__main__":
+    print("roll = ", roll)
+    kept = roll[:2].copy()
+    print("kept = ", kept)
+
+    # second roll
+    print("2. roll!")
+    roll = np.zeros(6, dtype=int)
+    for i in range(len(dice) - len(kept)):
+            die = np.random.choice(dice)
+            roll.put(i, die)
+
+    print("roll = ", roll)
+    kept = np.append(kept, roll[:2].copy())
+    print("kept = ", kept)
+
+    # third roll
+    print("3. roll!")
+    roll = np.zeros(6, dtype=int)
+    for i in range(len(dice) - len(kept)):
+            die = np.random.choice(dice)
+            roll.put(i, die)
+
+    print("roll = ", roll)
+    kept = np.append(kept, roll[:2].copy())
+    print("kept = ", kept)
+
+if __name__ == "__main__":
     main()
